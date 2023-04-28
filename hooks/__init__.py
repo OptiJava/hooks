362f0fdb9a851b9b5a45b1c8a092095732581b1c
OptiJava <OptiJava@protonmail.com>
2023-04-28 21:45:18 +0800

    add list

--- hooks/__init__.py
+++ hooks/__init__.py
@@ -1,4 +1,5 @@
 import os
+import tarfile
 from enum import Enum
 from typing import Dict, List
 
@@ -28,7 +29,7 @@ class Hooks(Enum):
     
     on_info = 'on_info'
     on_user_info = 'on_user_info'
-    
+
 
 class TaskType(Enum):
     undefined = 'undefined'
@@ -36,7 +37,7 @@ class TaskType(Enum):
     shell_command = 'shell_command'
     server_command = 'server_command'
     mcdr_command = 'mcdr_command'
-    
+
 
 class Task:
     name: str = 'undefined'
@@ -53,7 +54,8 @@ class Task:
     @new_thread('hooks - execute')
     def execute_task(self, server: PluginServerInterface, hook: str):
         if self.task_type == TaskType.undefined:
-            server.logger.error(f'Task state is not correct! Task: {self.name} Hooks: {hook} TaskType: {self.task_type} command: {self.command}')
+            server.logger.error(
+                f'Task state is not correct! Task: {self.name} Hooks: {hook} TaskType: {self.task_type} command: {self.command}')
             return
         
         if self.task_type == TaskType.shell_command:
@@ -79,10 +81,10 @@ class Configuration(Serializable):
         'on_server_crashed': [],
         'on_mcdr_started': [],
         'on_mcdr_stopped': [],
-
+        
         'on_player_joined': [],
         'on_player_left': [],
-
+        
         'on_info': [],
         'on_user_info': []
     }
@@ -92,6 +94,7 @@ class Configuration(Serializable):
 
 config: Configuration
 
+
 def trigger_hooks(hook: Hooks, server: PluginServerInterface):
     server.logger.debug(f'Triggered hooks {hook.value}')
     
@@ -99,12 +102,12 @@ def trigger_hooks(hook: Hooks, server: PluginServerInterface):
         for i in config.hooks.get(hook.value):
             server.logger.debug(f'Executing task {i}')
             config.task.get(i).execute_task(server, hook.value)
-            
-            
+
+
 ##################################################################
 ######################### For Commands ###########################
 ##################################################################
-            
+
 def mount_task(hook: str, task: str, src: CommandSource, server: PluginServerInterface):
     h = config.hooks.get(hook)
     
@@ -118,8 +121,8 @@ def mount_task(hook: str, task: str, src: CommandSource, server: PluginServerInt
     
     src.reply(RTextMCDRTranslation('hooks.mount.success', task, hook))
     h.append(task)
-    
-    
+
+
 def unmount_task(hook: str, task: str, src: CommandSource, server: PluginServerInterface):
     h = config.hooks.get(hook)
     
@@ -130,11 +133,11 @@ def unmount_task(hook: str, task: str, src: CommandSource, server: PluginServerI
     if task not in h:
         src.reply(RTextMCDRTranslation('hooks.mount.task_not_exist', task))
         return
-
+    
     src.reply(RTextMCDRTranslation('hooks.mount.unmount', hook, task))
     h.remove(task)
-    
-    
+
+
 def create_task(task_type: str, command: str, name: str, src: CommandSource, server: PluginServerInterface):
     if name in config.task:
         src.reply(RTextMCDRTranslation('hooks.create.already_exist'))
@@ -142,9 +145,14 @@ def create_task(task_type: str, command: str, name: str, src: CommandSource, ser
     
     server.logger.info(server.rtr('hooks.create.success', name))
     src.reply(RTextMCDRTranslation('hooks.create.success', name))
-
-    config.task[name] = Task(name, TaskType(task_type), command)
     
+    config.task[name] = Task(name, TaskType(task_type), command)
+
+
+def list(src: CommandSource, server: PluginServerInterface):
+    # TODO
+    pass
+
 
 ##################################################################
 ############################ Triggers ############################
@@ -190,50 +198,55 @@ def on_load(server: PluginServerInterface, old_module):
                 )
             )
         )
+        .then(
+            Literal('list')
+            .runs(lambda src: list(src, server))
+        )
     )
     
     trigger_hooks(Hooks.on_plugin_loaded, server)
-    
-    
+
+
 def on_unload(server: PluginServerInterface):
     trigger_hooks(Hooks.on_plugin_unloaded, server)
     
     server.save_config_simple(config)
-    
-    
+
+
 def on_info(server: PluginServerInterface, info: Info):
     trigger_hooks(Hooks.on_info, server)
-    
-    
+
+
 def on_user_info(server: PluginServerInterface, info: Info):
     trigger_hooks(Hooks.on_user_info, server)
-    
+
 
 def on_player_joined(server: PluginServerInterface, player: str, info: Info):
     trigger_hooks(Hooks.on_player_joined, server)
-    
-    
+
+
 def on_player_left(server: PluginServerInterface, player: str):
     trigger_hooks(Hooks.on_player_left, server)
-    
-    
+
+
 def on_server_start(server: PluginServerInterface):
     trigger_hooks(Hooks.on_server_starting, server)
-    
-    
+
+
 def on_server_startup(server: PluginServerInterface):
     trigger_hooks(Hooks.on_mcdr_started, server)
-    
-    
+
+
 def on_server_stop(server: PluginServerInterface, return_code: int):
     if return_code != 0:
         trigger_hooks(Hooks.on_server_crashed, server)
     else:
         trigger_hooks(Hooks.on_server_stopped, server)
 
+
 def on_mcdr_start(server: PluginServerInterface):
     trigger_hooks(Hooks.on_mcdr_started, server)
-    
-    
+
+
 def on_mcdr_stop(server: PluginServerInterface):
     trigger_hooks(Hooks.on_mcdr_stopped, server)
