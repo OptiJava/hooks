660d1988c59f057b399e998f9b5d2457288100da
OptiJava <OptiJava@protonmail.com>
2023-05-18 20:33:26 +0800

    add scripts & small refactor

--- hooks/__init__.py
+++ hooks/__init__.py
@@ -3,15 +3,16 @@ import os
 import time
 from enum import Enum
 from io import StringIO
-from typing import List, Any
+from typing import List, Any, Union
 
 from mcdreforged.api.all import *
 from mcdreforged.api.utils import serializer
+from ruamel import yaml
 
+scripts_folder: str = ''
 
-##################################################################
-##################### Basic Function #############################
-##################################################################
+scripts_list: dict[str, str] = {}
+
 
 class Hooks(Enum):
     undefined = 'undefined'
@@ -75,14 +76,12 @@ class Task(Serializable):
                     command.write('"')
                     command.write(str(var_dict.get(key)))
                     command.write('" && ')
-            
             command.write(self.command)
             
             os.system(command.getvalue())
         elif self.task_type == TaskType.server_command:
             # 替换参数
             command = self.command
-            
             if var_dict is not None:
                 for key in var_dict.keys():
                     command = command.replace('{$' + key + '}', str(var_dict.get(key)))
@@ -91,7 +90,6 @@ class Task(Serializable):
         elif self.task_type == TaskType.mcdr_command:
             # 替换参数
             command = self.command
-            
             if var_dict is not None:
                 for key in var_dict.keys():
                     command = command.replace('{$' + key + '}', str(var_dict.get(key)))
@@ -103,11 +101,16 @@ class Task(Serializable):
 
 
 class Configuration(Serializable):
-    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
     
     automatically: bool = True
+
+
+class TempConfig(Serializable):
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
     
     hooks: dict[str, List[str]] = {
         'undefined': [],
@@ -132,6 +135,8 @@ class Configuration(Serializable):
     task: dict[str, Task] = {}
 
 
+temp_config: TempConfig = TempConfig()
+
 config: Configuration
 
 
@@ -142,7 +147,7 @@ def trigger_hooks(hook: Hooks, server: PluginServerInterface, objects_dict: dict
     try:
         server.logger.debug(f'Triggered hooks {hook.value}')
         server.logger.debug(f'objects_dict: {str(objects_dict)}')
-        if len(config.hooks.get(hook.value)) != 0:
+        if len(temp_config.hooks.get(hook.value)) != 0:
             _trigger_hooks(hook, server, objects_dict)
     except Exception as e:
         server.logger.exception(f'Unexpected exception when triggering hook {hook.value}', e)
@@ -173,30 +178,25 @@ def _trigger_hooks(hook: Hooks, server: PluginServerInterface, objects_dict: dic
                 var_inner_attr_value: Any = var_inner_attr_dict.get(var_inner_attr_key)
                 
                 finally_var_dict[an_object_key + '_' + var_inner_attr_key] = var_inner_attr_value
-        
+    
     server.logger.debug(f'Executing hook {hook.value}')
     # 遍历被挂载到此hook的task的key
-    for task in config.hooks.get(hook.value):
-        if config.task.get(task) is None:
+    for task in temp_config.hooks.get(hook.value):
+        if temp_config.task.get(task) is None:
             server.logger.warning(f'Task {task} is not exist, unmount it from hook {hook.value}!')
-            config.hooks.get(hook.value).remove(task)
+            temp_config.hooks.get(hook.value).remove(task)
             return
         # 执行任务
         try:
-            config.task.get(task).execute_task(server, hook.value, finally_var_dict)
+            temp_config.task.get(task).execute_task(server, hook.value, finally_var_dict)
         except Exception as e:
             server.logger.exception(
-                f'Unexpected exception when executing task {task}, hook {hook.value}, task_type {config.task.get(task).task_type}, command {config.task.get(task).command}',
+                f'Unexpected exception when executing task {task}, hook {hook.value}, task_type {temp_config.task.get(task).task_type}, command {temp_config.task.get(task).command}',
                 e)
 
 
-##################################################################
-######################### For Commands ###########################
-##################################################################
-
-@new_thread('hooks - mount')
 def mount_task(hook: str, task: str, src: CommandSource, server: PluginServerInterface):
-    h = config.hooks.get(hook)
+    h = temp_config.hooks.get(hook)
     
     if h is None:
         src.reply(RTextMCDRTranslation('hooks.mount.hook_not_exist', hook))
@@ -211,9 +211,8 @@ def mount_task(hook: str, task: str, src: CommandSource, server: PluginServerInt
     server.logger.info(f'Successfully mounted task {task}')
 
 
-@new_thread('hooks - unmount')
 def unmount_task(hook: str, task: str, src: CommandSource, server: PluginServerInterface):
-    h = config.hooks.get(hook)
+    h = temp_config.hooks.get(hook)
     
     if h is None:
         src.reply(RTextMCDRTranslation('hooks.mount.hook_not_exist', hook))
@@ -228,9 +227,8 @@ def unmount_task(hook: str, task: str, src: CommandSource, server: PluginServerI
     server.logger.info(f'Successfully unmounted task {task}')
 
 
-@new_thread('hooks - create')
 def create_task(task_type: str, command: str, name: str, src: CommandSource, server: PluginServerInterface):
-    if name in config.task:
+    if name in temp_config.task:
         src.reply(RTextMCDRTranslation('hooks.create.already_exist'))
         return
     
@@ -240,34 +238,33 @@ def create_task(task_type: str, command: str, name: str, src: CommandSource, ser
         src.reply(RTextMCDRTranslation('hooks.create.task_type_wrong', task_type))
         return
     
-    config.task[name] = Task(name=name, task_type=tsk_type, command=command)
+    temp_config.task[name] = Task(name=name, task_type=tsk_type, command=command)
     
     server.logger.info(f'Successfully created task {name}')
     src.reply(RTextMCDRTranslation('hooks.create.success', name))
 
 
-@new_thread('hooks - delete')
 def delete_task(name: str, src: CommandSource, server: PluginServerInterface):
-    if name not in config.task.keys():
+    if name not in temp_config.task.keys():
         src.reply(RTextMCDRTranslation('hooks.mount.task_not_exist', name))
         return
     
     server.logger.info(f'Successfully deleted task {name}')
     src.reply(RTextMCDRTranslation('hooks.delete.success', name))
     
-    config.task.pop(name)
+    temp_config.task.pop(name)
 
 
 @new_thread('hooks - list')
 def list_task(src: CommandSource):
     rtext_list = RTextList()
     
-    if len(config.task.values()) == 0:
+    if len(temp_config.task.values()) == 0:
         rtext_list.append(RText('Nothing', color=RColor.dark_gray, styles=RStyle.italic))
         src.reply(RTextMCDRTranslation('hooks.list.task', rtext_list))
         return
     
-    for t in config.task.values():
+    for t in temp_config.task.values():
         rtext_list.append(RText(t.name + '  ', color=RColor.red).h(t.task_type.name + ' -> ' + t.command))
     
     src.reply(RTextMCDRTranslation('hooks.list.task', rtext_list))
@@ -277,31 +274,35 @@ def list_task(src: CommandSource):
 def list_mount(src: CommandSource):
     src.reply(
         RTextMCDRTranslation('hooks.list.mount',
-                             config.hooks.get(Hooks.on_plugin_loaded.value),
-                             config.hooks.get(Hooks.on_plugin_unloaded.value),
-                             config.hooks.get(Hooks.on_server_starting.value),
-                             config.hooks.get(Hooks.on_server_started.value),
-                             config.hooks.get(Hooks.on_server_stopped.value),
-                             config.hooks.get(Hooks.on_server_crashed.value),
-                             config.hooks.get(Hooks.on_mcdr_started.value),
-                             config.hooks.get(Hooks.on_mcdr_stopped.value),
-                             config.hooks.get(Hooks.on_player_joined.value),
-                             config.hooks.get(Hooks.on_player_left.value),
-                             config.hooks.get(Hooks.on_info.value),
-                             config.hooks.get(Hooks.on_user_info.value),
-                             config.hooks.get(Hooks.undefined.value),
+                             temp_config.hooks.get(Hooks.on_plugin_loaded.value),
+                             temp_config.hooks.get(Hooks.on_plugin_unloaded.value),
+                             temp_config.hooks.get(Hooks.on_server_starting.value),
+                             temp_config.hooks.get(Hooks.on_server_started.value),
+                             temp_config.hooks.get(Hooks.on_server_stopped.value),
+                             temp_config.hooks.get(Hooks.on_server_crashed.value),
+                             temp_config.hooks.get(Hooks.on_mcdr_started.value),
+                             temp_config.hooks.get(Hooks.on_mcdr_stopped.value),
+                             temp_config.hooks.get(Hooks.on_player_joined.value),
+                             temp_config.hooks.get(Hooks.on_player_left.value),
+                             temp_config.hooks.get(Hooks.on_info.value),
+                             temp_config.hooks.get(Hooks.on_user_info.value),
+                             temp_config.hooks.get(Hooks.undefined.value),
                              )
     )
 
 
 def reload_config(src: CommandSource, server: PluginServerInterface):
-    server.load_config_simple(target_class=Configuration)
+    global config, temp_config
+    
+    temp_config = TempConfig()
+    config = server.load_config_simple(target_class=Configuration)
+    load_scripts(server)
     server.logger.info('Config reloaded.')
     src.reply(RTextMCDRTranslation('hooks.reload.success'))
-    
+
 
 def man_run_task(task: str, env_str: str, src: CommandSource, server: PluginServerInterface):
-    if task not in config.task.keys():
+    if task not in temp_config.task.keys():
         src.reply(RTextMCDRTranslation('hooks.man_run.task_not_exist'))
         return
     
@@ -312,12 +313,64 @@ def man_run_task(task: str, env_str: str, src: CommandSource, server: PluginServ
         return
     
     try:
-        config.task.get(task).execute_task(server, Hooks.undefined.value, env_dict)
+        temp_config.task.get(task).execute_task(server, Hooks.undefined.value, env_dict)
         src.reply(RTextMCDRTranslation('hooks.man_run.success', task))
     except Exception as e:
         server.logger.exception(
-            f'Unexpected exception when executing task {task}, hook {Hooks.undefined.value}, task_type {config.task.get(task).task_type}, command {config.task.get(task).command}',
+            f'Unexpected exception when executing task {task}, hook {Hooks.undefined.value}, task_type {temp_config.task.get(task).task_type}, command {temp_config.task.get(task).command}',
             e)
+
+
+def register_scripts(script_path: str):
+    # 将绝对路径添加进入script_list
+    scripts_list[os.path.basename(script_path)] = script_path
+
+
+def parse_and_load_scripts(script: str, server: PluginServerInterface):
+    # 读取
+    with open(scripts_list.get(script), 'r') as f:
+        content: dict[str, Union[str, Union[list, dict]]] = yaml.load(f.read(), Loader=yaml.Loader)
+    
+    for task in content.get('tasks').values():
+        # 创建task
+        create_task(task.get('task_type'), task.get('command'), task.get('name'), server.get_plugin_command_source(),
+                    server)
+        for hook in task.get('hooks'):
+            # 挂载
+            mount_task(hook, task.get('name'), server.get_plugin_command_source(), server)
+
+
+def load_scripts(server: PluginServerInterface):
+    global scripts_list
+    
+    if not os.path.isdir(scripts_folder):
+        # 创建脚本目录
+        os.makedirs(scripts_folder)
+        return
+    
+    def list_all_files(root_dir) -> list[str]:
+        # 显示一个文件夹及子文件夹中的所有yaml文件
+        _files_in_a_folder: list[str] = []
+        
+        for file in os.listdir(root_dir):
+            file_path = os.path.join(root_dir, file)
+            
+            if os.path.isdir(file_path):
+                # 遍历子文件夹
+                _files_in_a_folder.extend(list_all_files(file_path))
+            if os.path.isfile(file_path) and (file_path.endswith('.yaml') or file_path.endswith('.yml')):
+                # 添加文件路径
+                _files_in_a_folder.append(file_path)
+        
+        return _files_in_a_folder
+    
+    # 遍历所有文件
+    for script_path in list_all_files(scripts_folder):
+        register_scripts(script_path)
+    
+    # 遍历所有已成功注册的脚本
+    for script in scripts_list.keys():
+        parse_and_load_scripts(script, server)
 
 
 def process_arg_server(server: PluginServerInterface) -> PluginServerInterface:
@@ -337,14 +390,15 @@ def process_arg_server(server: PluginServerInterface) -> PluginServerInterface:
     return server
 
 
-##################################################################
-############################ Triggers ############################
-##################################################################
-
 def on_load(server: PluginServerInterface, old_module):
-    global config
+    global config, scripts_folder, temp_config
+    
+    temp_config = TempConfig()
     
     config = server.load_config_simple(target_class=Configuration)
+    
+    scripts_folder = os.path.join(server.get_data_folder(), 'scripts')
+    load_scripts(server)
     
     server.register_command(
         Literal('!!hooks')
